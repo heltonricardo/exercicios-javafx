2beaca82fb56680365a6c3659650270b9683b1de
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-11-24 13:39:01 -0300

    Update AppLayout.java

--- src/layout/AppLayout.java
+++ src/layout/AppLayout.java
@@ -1,6 +1,7 @@
 package layout;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class AppLayout extends Application {
@@ -8,6 +9,9 @@ public class AppLayout extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
+		Scene principal = new Scene(new TesteAnchorPane(), 800, 600);
+		
+		primaryStage.setScene(principal);
 		primaryStage.setTitle("Gerenciadores de Layout");
 		primaryStage.show();
 	}
