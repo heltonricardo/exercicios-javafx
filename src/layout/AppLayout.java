1fb74766bc8caae240a4f22646702154d00da305
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-11-24 14:43:12 -0300

    Update AppLayout.java

--- src/layout/AppLayout.java
+++ src/layout/AppLayout.java
@@ -13,7 +13,8 @@ public class AppLayout extends Application {
 		Parent raiz = null;
 
 		//raiz = new TesteAnchorPane();
-		raiz = new TesteBorderPane();
+		//raiz = new TesteBorderPane();
+		raiz = new TesteFlowPane();
 		
 		Scene principal = new Scene(raiz, 800, 600);
 		primaryStage.setScene(principal);
