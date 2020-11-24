cf06eee89b619245f82c672ba711d3241cc5185b
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-11-24 13:38:52 -0300

    Update AppLayout.java

--- src/layout/Quadrado.java
+++ src/layout/Quadrado.java
@@ -5,7 +5,7 @@ import javafx.scene.shape.Rectangle;
 
 public class Quadrado extends Rectangle {
 
-private static int i = 0;
+	private static int i = 0;
 	
 	private String[] cores = {
 		"#c33c5e", "#39aac6", "#28d79a",
@@ -13,12 +13,12 @@ private static int i = 0;
 	};
 
 	public Quadrado() {
-		this(100, 100);
+		this(100);
 	}
 	
-	public Quadrado(int largura, int altura) {
-		setWidth(largura);
-		setHeight(altura);
+	public Quadrado(int tamanho) {
+		setWidth(tamanho);
+		setHeight(tamanho);
 		
 		setFill(Color.web(cores[i++ % 6]));
 	}
