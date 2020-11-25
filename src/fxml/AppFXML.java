eb70ef17bd750c687a5ee7aa7fea8e4d96a16800
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-11-25 14:07:54 -0300

    Create AppFXML.java

---
+++ src/fxml/AppFXML.java
@@ -0,0 +1,17 @@
+package fxml;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppFXML extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
